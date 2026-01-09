e2e32b48efad151ce831171b9d68300996e5cd0d
Denratnegeiten <aagatius@gmail.com>
2026-01-09 17:46:26 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Страница первого объекта */
+router.get('/alb1', function(req, res, next) {
+  res.send("<h1>Страница Альбома 1</h1>")
+});
+
+/* Страница второго объекта */
+router.get('/alb2', function(req, res, next) {
+  res.send("<h1>Страница Альбома 2</h1>")
+});
+
+/* Страница третьего объекта */
+router.get('/alb3', function(req, res, next) {
+  res.send("<h1>Страница Альбома 3</h1>")
 });
 
 module.exports = router;

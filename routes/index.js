2f565a5c2b62c887ec67d388faeae0cf46c051a1
Denratnegeiten <aagatius@gmail.com>
2026-01-09 19:17:53 +0300

    windir_4_4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index'); 
+res.render('index', { 
+    title: 'Windir Legacy',
+    subtitle: 'Наследие Согнаметала',
+    albums: ['Likferd', '1184', 'Arntor', 'Sóknardalr']
+  });
 });
 
 module.exports = router;

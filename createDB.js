eb4865e873504d651f6b584ad8b4515d6963fb10
Denratnegeiten <aagatius@gmail.com>
2026-01-09 21:46:57 +0300

    windir_6_3

--- createDB.js
+++ createDB.js
@@ -1,21 +1,24 @@
 const { MongoClient } = require('mongodb');
 var data = require("./data.js").data;
 
-console.log(data);
-
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
-const dbName = 'test2024';
+
+const dbName = 'tc2024';
 
 async function main() {
   await client.connect();
   console.log('Connected successfully to server');
+  
   const db = client.db(dbName);
   
-  const collection = db.collection('documents');
+  const collection = db.collection('cats');
+  
+  try { await collection.drop(); } catch (e) { console.log("Collection empty, starting fresh"); }
+
   const insertResult = await collection.insertMany(data);
   console.log('Inserted documents =>', insertResult);
-
+  
   return 'done.';
 }
 

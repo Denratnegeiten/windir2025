55f526fb7f3ee8eb386c06b74877369d7fcff204
Denratnegeiten <aagatius@gmail.com>
2026-01-09 21:43:10 +0300

    windir_6_2

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = [{
+   title: 'Terje Bakken',
+   nick: 'valfar',
+   avatar: '/images/valfar.jpg',
+   desc: 'Основатель и вокалист Windir, известный как Valfar. Создатель стиля Sognametal.'
+},
+{
+   title: '1184',
+   nick: 'album_1184',
+   avatar: '/images/1184.jpg',
+   desc: 'Третий студийный альбом, записанный совместно с группой Ulcus.'
+},
+{
+   title: 'Likferd',
+   nick: 'likferd',
+   avatar: '/images/likferd.jpg',
+   desc: 'Последний альбом группы, выпущенный при жизни Вальфара в 2003 году.'
+}
+];
+
+module.exports.data = data;

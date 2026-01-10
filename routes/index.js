e46c695494c232cbdf4fc2e1abe7acb13005f270
Denratnegeiten <aagatius@gmail.com>
2026-01-10 18:16:49 +0300

    windir_9_2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { 
+  res.cookie('greeting', 'Welcome to Sognametal').render('index', {
     title: 'Windir Legacy',
     subtitle: 'Наследие Согнаметала',
     albums: ['Likferd', '1184', 'Arntor', 'Sóknardalr']

b7b1cf455e6a7230b9775822b3d403ce0b67006d
Denratnegeiten <aagatius@gmail.com>
2026-01-10 17:54:58 +0300

    windir_8_4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-res.render('index', { 
+  res.render('index', { 
     title: 'Windir Legacy',
     subtitle: 'Наследие Согнаметала',
     albums: ['Likferd', '1184', 'Arntor', 'Sóknardalr']
@@ -14,18 +13,13 @@ router.get('/biography', (req, res) => res.render('biography', { title: 'Био�
 router.get('/discography', (req, res) => res.render('discography', { title: 'Дискография Windir' }));
 router.get('/gallery', (req, res) => res.render('gallery', { title: 'Галлерея Windir' }));
 
-/* Обработка формы подписки */
-/* Страница подтверждения (GET) */
 router.get('/success', function(req, res) {
   res.render('success');
 });
 
-/* Обработка формы (POST) */
 router.post('/subscribe', function(req, res) {
   const email = req.body.user_email;
   console.log("Подписан новый адрес:", email);
-  
-  // Вместо res.send используем редирект на созданный выше адрес
   res.redirect('/success'); 
 });
 

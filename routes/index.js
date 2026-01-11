1a438124ccfa52b504ea4ab81d86cfa8e77a9224
Denratnegeiten <aagatius@gmail.com>
2026-01-11 07:01:41 +0300

    windir_10_4

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user').User;
 
 router.get('/', function(req, res, next) {
   req.session.greeting = "Valfar is watching over the session";
@@ -29,13 +30,35 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg', { title: 'Вход' });
 });
 
-router.post('/logreg', function(req, res, next) {
+router.post('/logreg', async function(req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   
   console.log("Логин из формы:", username);
-  console.log("Пароль из формы:", password);
   
+  // Ищем пользователя в базе данных
+  var users = await User.find({username: username});
+
+  if (!users.length) {
+    // Если пользователя нет — создаем нового
+    var user = new User({username: username, password: password});
+    await user.save();
+    
+    // Сохраняем ID в сессию и редиректим на главную
+    req.session.user_id = user._id;
+    res.redirect('/');
+  } else {
+    // Если пользователь найден — проверяем пароль
+    var foundUser = users[0];
+    if (foundUser.checkPassword(password)) {
+      // Пароль верный — сохраняем ID и на главную
+      req.session.user_id = foundUser._id;
+      res.redirect('/');
+    } else {
+      // Пароль неверный — возвращаем на страницу входа
+      res.render('logreg', { title: 'Вход' });
+    }
+  }
 });
 
 module.exports = router;

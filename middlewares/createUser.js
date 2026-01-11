7ac631368e2c2e91048514de5ebfcecf2bac449b
Denratnegeiten <aagatius@gmail.com>
2026-01-11 08:23:12 +0300

    windir_10_8

--- middlewares/createUser.js
+++ middlewares/createUser.js
@@ -1,17 +1,16 @@
 var User = require("../models/user").User;
 
 module.exports = async function(req, res, next) {
-  res.locals.user = null; // По умолчанию пользователя нет
+  res.locals.user = null;
 
   if (!req.session.user_id) {
     return next();
   }
 
   try {
-    // Ищем пользователя по ID из сессии
     var user = await User.findById(req.session.user_id);
     if (user) {
-      res.locals.user = user; // Передаем данные пользователя во все шаблоны
+      res.locals.user = user;
     }
     next();
   } catch (err) {
